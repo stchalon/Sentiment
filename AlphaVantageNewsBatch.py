cd77ccb9bd56f1abe08a7c34761c5be72afe1dd9
stchalon <65845891+stchalon@users.noreply.github.com>
2025-04-10 16:31:51 +0200

    Update AlphaVantageNewsBatch.py

--- AlphaVantageNewsBatch.py
+++ AlphaVantageNewsBatch.py
@@ -5,11 +5,11 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 # Alpha Vantage API Key
-api_key = "PM3TEJGFH2WHWPIG"
+api_key = "<your API key>"
 
 # Start date for news retrieval (50 news maximum)
 # [2025-01-01, 2025-08-04] done
-# [2024-09-01, 2024-12-31]
+# [2024-09-01, 2024-12-31] done
 start_date = datetime(2024, 9, 1)  # to update to starting date
 end_date = datetime(2025, 12, 31)    # to update to ending date
 #end_date = datetime.today()     # to update to ending date
@@ -42,9 +42,6 @@ while current_date < end_date and requests_made < max_requests_per_day:
     # Make request
     response = requests.get(url)
     data = response.json()
-    # data["feed"] to see the news items
-    # data["feed"][2] to see the second news item
-    # data["feed"][1]["title"]  to see the title of the news
 
     if "feed" in data:
         for i, item in enumerate(data["feed"], start=1):
@@ -68,24 +65,3 @@ while current_date < end_date and requests_made < max_requests_per_day:
 
 print("Finished retrieving historical news.")
 
-# write to file
-# The most obvious way to do this would be to print to a file object:
-#
-##with open("test.txt", "a") as myfile:
-##    myfile.write("appended text")
-#with open('out.txt', 'w') as f:
-#    print('Filename:', filename, file=f)  # Python 3.x
-#    # print >> f, 'Filename:', filename   # Python 2.x
-#However, redirecting stdout also works for me. It is probably fine for a one-off script such as this:
-#
-#import sys
-#
-#orig_stdout = sys.stdout
-#f = open('out.txt', 'w')
-#sys.stdout = f
-
-#for i in range(2):
-#    print('i = ', i)
-#
-#sys.stdout = orig_stdout
-#f.close()
